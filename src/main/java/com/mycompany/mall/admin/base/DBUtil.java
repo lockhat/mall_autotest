302f08ffd4197bf979095ddf433ec1e79a8c6796
lockhat <15020955+lockhat@user.noreply.gitee.com>
2025-03-30 22:02:13 +0800

    Druid配置

--- src/main/java/com/mycompany/mall/admin/base/DBUtil.java
+++ src/main/java/com/mycompany/mall/admin/base/DBUtil.java
@@ -4,13 +4,13 @@ package com.mycompany.mall.admin.base;
  * @Author: Liu Yue
  * @Date: 2025/3/30 下午4:57
  */
+
 import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.BeanListHandler;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.commons.dbutils.handlers.*;
 
 import javax.sql.DataSource;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -19,53 +19,46 @@ public class DBUtil {
     private static DataSource dataSource;
 
     static {
-        // 加载数据库配置（从 properties 文件或直接配置）
-        Properties props = new Properties();
-        props.setProperty("driverClassName", "com.mysql.cj.jdbc.Driver");
-        props.setProperty("url", "jdbc:mysql://60.204.173.174:3306/mall?useSSL=false");
-        props.setProperty("username", "reader");
-        props.setProperty("password", "123456");
-        props.setProperty("initialSize", "5");
-        props.setProperty("maxActive", "20");
+        try {
+            // 1. 读取环境变量（优先用 -Denv=test 传参，否则默认 test）
+            String env = System.getProperty("env", "test"); // 可设置为 dev/test/uat/prod
 
-        dataSource = new DruidDataSource();
-        ((DruidDataSource) dataSource).setDriverClassName(props.getProperty("driverClassName"));
-        ((DruidDataSource) dataSource).setUrl(props.getProperty("url"));
-        ((DruidDataSource) dataSource).setUsername(props.getProperty("username"));
-        ((DruidDataSource) dataSource).setPassword(props.getProperty("password"));
-        ((DruidDataSource) dataSource).setInitialSize(Integer.parseInt(props.getProperty("initialSize")));
-        ((DruidDataSource) dataSource).setMaxActive(Integer.parseInt(props.getProperty("maxActive")));
+            String fileName = String.format("db-%s.properties", env);
+            InputStream is = DBUtil.class.getClassLoader().getResourceAsStream(fileName);
+            if (is == null) {
+                throw new RuntimeException("未找到数据库配置文件: " + fileName);
+            }
+
+            Properties props = new Properties();
+            props.load(is);
+
+            DruidDataSource ds = new DruidDataSource();
+            ds.setDriverClassName(props.getProperty("driverClassName"));
+            ds.setUrl(props.getProperty("url"));
+            ds.setUsername(props.getProperty("username"));
+            ds.setPassword(props.getProperty("password"));
+            ds.setInitialSize(Integer.parseInt(props.getProperty("initialSize", "5")));
+            ds.setMaxActive(Integer.parseInt(props.getProperty("maxActive", "20")));
+
+            dataSource = ds;
+        } catch (Exception e) {
+            throw new RuntimeException("初始化数据库连接池失败", e);
+        }
     }
 
-    /**
-     * 执行查询（返回单个对象）
-     */
     public static <T> T queryForObject(String sql, Class<T> clazz, Object... params) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        return runner.query(sql, new BeanHandler<>(clazz), params);
+        return new QueryRunner(dataSource).query(sql, new BeanHandler<>(clazz), params);
     }
 
-    /**
-     * 执行查询（返回列表）
-     */
     public static <T> List<T> queryForList(String sql, Class<T> clazz, Object... params) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        return runner.query(sql, new BeanListHandler<>(clazz), params);
+        return new QueryRunner(dataSource).query(sql, new BeanListHandler<>(clazz), params);
     }
 
-    /**
-     * 执行更新（INSERT/UPDATE/DELETE）
-     */
     public static int update(String sql, Object... params) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        return runner.update(sql, params);
+        return new QueryRunner(dataSource).update(sql, params);
     }
 
-    /**
-     * 执行聚合查询（返回单个值，如 COUNT(*)）
-     */
     public static Object queryForScalar(String sql, Object... params) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        return runner.query(sql, new ScalarHandler<>(), params);
+        return new QueryRunner(dataSource).query(sql, new ScalarHandler<>(), params);
     }
 }
